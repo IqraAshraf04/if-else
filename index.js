ce4fe90a2f5471560d8b5581f23029cc7adfd0a1
Iqra <iwrites04@gmail.com>
2022-05-12 10:03:51 +0500

    updated

---
+++ index.js
@@ -0,0 +1,16 @@
+// var a = 4;
+// var b = 6;
+// document.write(a+b)
+
+// let a = 3;
+// let b = 4;
+// document.write(a+b)
+
+// const a = 4;
+// const b = 3;
+// document.write(a+b+a)
+
+// let a = 3;
+// let b = 6;
+// let z = a+b;
+// console.log(z+z);
